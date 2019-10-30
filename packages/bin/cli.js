cbfd9023ec7b1a0005af10ec03cd8d8e420ea072
JounQin <admin@1stg.me>
2019-10-30 09:46:40 +0800

    fix(bin): cache cli error

--- packages/bin/cli.js
+++ packages/bin/cli.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 const { resolve } = require('path')
-module.exports = require('./lib/cjs')(resolve('node_modules/@d-ts'))
+
+try {
+  module.exports = require('./lib/cjs')(resolve('node_modules/@d-ts'))
+} catch (e) {}
