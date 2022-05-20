680a71c4cfcd3a9521f8a18ff777ad43fdea5bb5
JounQin <admin@1stg.me>
2022-05-21 00:02:34 +0800

    docs: add changeset for releasing

---
+++ .changeset/blue-laws-build.md
@@ -0,0 +1,8 @@
+---
+'@d-ts/bin': patch
+'@d-ts/core': patch
+'@d-ts/svelte': patch
+'@d-ts/webpack-plugins': patch
+---
+
+fix: jsox/svelte type issues
--- packages/bin/src/index.ts
+++ packages/bin/src/index.ts
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 
 const link = (dtsDir: string, pkgName: string) => {
   if (pkgName === 'bin') {
